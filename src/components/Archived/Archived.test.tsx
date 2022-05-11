6bc770b3de5db7ad19fb57c38631b6b05c5f2672
YaBoiBreezy <alexbreeze01@gmail.com>
2022-05-11 15:32:20 -0400

    nothing showed up, trying this

---
+++ src/components/Archived/Archived.test.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import Archived from '@components/Archived';
+
+describe('Archived', () => {
+  const ArchivedContent = 'Hello world!';
+  const ArchivedAdditionalContent = 'addon';
+  test('renders default english Archived component', () => {
+    render(
+      <Archived content="">
+        <p>{ArchivedContent}</p>
+      </Archived>
+    );
+    expect(screen.getByText(ArchivedContent)).toBeInTheDocument();
+  });
+  test('renders default french Archived component', () => {
+    render(
+      <Archived content="" french>
+        <p>{ArchivedContent}</p>
+      </Archived>
+    );
+    expect(screen.getByText(ArchivedContent)).toBeInTheDocument();
+  });
+
+  test('renders default english Archived component with optional link', () => {
+    render(
+      <Archived content={ArchivedAdditionalContent}>
+        <p>{ArchivedContent}</p>
+      </Archived>
+    );
+    expect(screen.getByText(ArchivedContent)).toBeInTheDocument();
+    expect(screen.getByText(ArchivedAdditionalContent)).toBeInTheDocument();
+  });
+
+  test('renders default french Archived component with optional link', () => {
+    render(
+      <Archived content={ArchivedAdditionalContent} french>
+        <p>{ArchivedContent}</p>
+      </Archived>
+    );
+    expect(screen.getByText(ArchivedContent)).toBeInTheDocument();
+    expect(screen.getByText(ArchivedAdditionalContent)).toBeInTheDocument();
+  });
+});
