4553ebc6ae15b1a2c764054ef75d33125cca5878
YaBoiBreezy <alexbreeze01@gmail.com>
2022-05-13 14:39:45 -0400

    made attributes optional

--- src/components/KeyboardKey/KeyboardKey.tsx
+++ src/components/KeyboardKey/KeyboardKey.tsx
@@ -3,9 +3,9 @@ import '../../style.css';
 
 export interface KeyboardKeyProps extends React.HTMLAttributes<HTMLElement> {
   /** the key being displayed */
-  keyValue: string;
+  keyValue?: string;
   /** optional hidden description of key, for screen readers */
-  description: string;
+  description?: string;
 }
 
 const KeyboardKey = ({ keyValue = '', description = '' }: KeyboardKeyProps) => {
