538bb1e1bbfdffa12b777f413caa92858112ca8f
YaBoiBreezy <alexbreeze01@gmail.com>
2022-05-13 09:32:55 -0400

    improved heading, made issue in test idk how to fix

--- src/components/Heading/Heading.test.tsx
+++ src/components/Heading/Heading.test.tsx
@@ -1,41 +1,42 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+
 import Heading from '@components/Heading';
 
 describe('Heading', () => {
   const HeadingContent = 'Quote this';
   const HeadingSecondary = 'second';
 
-  describe('Test Heading sizes', () => {
+  describe('Test Heading levels', () => {
     test('render Heading h1', () => {
-      render(<Heading size="h1" text={HeadingContent} />);
+      render(<Heading level="h1" text={HeadingContent} />);
       expect(screen.getByText(HeadingContent)).toBeInTheDocument();
     });
     test('render Heading h2', () => {
-      render(<Heading size="h2" text={HeadingContent} />);
+      render(<Heading level="h2" text={HeadingContent} />);
       expect(screen.getByText(HeadingContent)).toBeInTheDocument();
     });
     test('render Heading h3', () => {
-      render(<Heading size="h3" text={HeadingContent} />);
+      render(<Heading level="h3" text={HeadingContent} />);
       expect(screen.getByText(HeadingContent)).toBeInTheDocument();
     });
     test('render Heading h4', () => {
-      render(<Heading size="h4" text={HeadingContent} />);
+      render(<Heading level="h4" text={HeadingContent} />);
       expect(screen.getByText(HeadingContent)).toBeInTheDocument();
     });
     test('render Heading h5', () => {
-      render(<Heading size="h5" text={HeadingContent} />);
+      render(<Heading level="h5" text={HeadingContent} />);
       expect(screen.getByText(HeadingContent)).toBeInTheDocument();
     });
     test('render Heading h6', () => {
-      render(<Heading size="h6" text={HeadingContent} />);
+      render(<Heading level="h6" text={HeadingContent} />);
       expect(screen.getByText(HeadingContent)).toBeInTheDocument();
     });
   });
   test('heading secondary text', () => {
     render(
       <Heading
-        size="h6"
+        level="h6"
         text={HeadingContent}
         secondaryText={HeadingSecondary}
       />
@@ -46,7 +47,7 @@ describe('Heading', () => {
   test('heading underlined', () => {
     render(
       <Heading
-        size="h6"
+        level="h6"
         text={HeadingContent}
         secondaryText={HeadingSecondary}
         underline
--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -4,8 +4,8 @@ import '../../style.css';
 type headingType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | undefined;
 
 export interface HeadingProps extends React.HTMLAttributes<HTMLElement> {
-  /** size of heading */
-  size?: headingType;
+  /** size of heading, default is h1 */
+  level?: headingType;
   /** Contents of Heading */
   children?: React.ReactNode;
   /** heading text */
@@ -17,68 +17,34 @@ export interface HeadingProps extends React.HTMLAttributes<HTMLElement> {
 }
 
 const Heading = ({
-  size = 'h1',
+  level = 'h1',
   secondaryText = '',
   underline = false,
   text = '',
 }: HeadingProps) => {
   const underlineString = underline ? 'page-header' : '';
+  const Tag =
+    level === 'h2'
+      ? 'h2'
+      : level === 'h3'
+      ? 'h3'
+      : level === 'h4'
+      ? 'h4'
+      : level === 'h5'
+      ? 'h5'
+      : level === 'h6'
+      ? 'h6'
+      : 'h1';
+
   return secondaryText === '' ? (
-    size === 'h1' ? (
-      <h1 className={underlineString}>{text}</h1>
-    ) : size === 'h2' ? (
-      <h2 className={underlineString}>{text}</h2>
-    ) : size === 'h3' ? (
-      <h3 className={underlineString}>{text}</h3>
-    ) : size === 'h4' ? (
-      <h4 className={underlineString}>{text}</h4>
-    ) : size === 'h5' ? (
-      <h5 className={underlineString}>{text}</h5>
-    ) : size === 'h6' ? (
-      <h6 className={underlineString}>{text}</h6>
-    ) : (
-      <p>ERROR: no size given for this header</p>
-    )
-  ) : size === 'h1' ? (
-    <h1 className={underlineString}>
-      {text}
-      {' – '}
-      <span className="small">{secondaryText}</span>
-    </h1>
-  ) : size === 'h2' ? (
-    <h2 className={underlineString}>
-      {text}
-      {' – '}
-      <span className="small">{secondaryText}</span>
-    </h2>
-  ) : size === 'h3' ? (
-    <h3 className={underlineString}>
-      {text}
-      {' – '}
-      <span className="small">{secondaryText}</span>
-    </h3>
-  ) : size === 'h4' ? (
-    <h4 className={underlineString}>
-      {text}
-      {' – '}
-      <span className="small">{secondaryText}</span>
-    </h4>
-  ) : size === 'h5' ? (
-    <h5 className={underlineString}>
-      {text}
-      {' – '}
-      <span className="small">{secondaryText}</span>
-    </h5>
-  ) : size === 'h6' ? (
-    <h6 className={underlineString}>
-      {text}
-      {' – '}
-      <span className="small">{secondaryText}</span>
-    </h6>
+    <Tag className={underlineString}>{text}</Tag>
   ) : (
-    <p>ERROR: no size given for this header</p>
+    <Tag className={underlineString}>
+      {text}
+      {' – '}
+      <span className="small">{secondaryText}</span>
+    </Tag>
   );
 };
-Heading.displayName = 'Heading';
 
 export default Heading;
