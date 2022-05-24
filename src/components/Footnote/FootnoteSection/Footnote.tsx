8765a431a5223d5a02ac7263ffb54cefd5a4fb8e
YaBoiBreezy <alexbreeze01@gmail.com>
2022-05-24 09:21:10 -0400

    fixed footnote linking, still missing programmatic thing to dynamically change footnote href to point to correct link (href=fn1-<dynamic value>-x)

--- src/components/Footnote/FootnoteSection/Footnote.tsx
+++ src/components/Footnote/FootnoteSection/Footnote.tsx
@@ -8,25 +8,22 @@ export interface FootnoteProps extends React.HTMLAttributes<HTMLElement> {
   children?: React.ReactNode;
 }
 
-const Footnote = ({ value = '', children }: FootnoteProps) => {
-  const footnoteId = `fn${value}-rf`;
-  return (
-    <span>
-      <dt>Footnote {value}</dt>
-      <dd id={footnoteId}>
-        {children}
-        <p className="fn-rtn">
-          <a href={footnoteId}>
-            <span className="wb-inv">
-              Return to <span>first</span> footnote
-            </span>
-            {value}
-            <span className="wb-inv"> referrer</span>
-          </a>
-        </p>
-      </dd>
-    </span>
-  );
-};
+const Footnote = ({ value = '', children }: FootnoteProps) => (
+  <span>
+    <dt>Footnote {value}</dt>
+    <dd id={`fn${value}`}>
+      {children}
+      <p className="fn-rtn">
+        <a href={`#fn${value}-rf`}>
+          <span className="wb-inv">
+            Return to <span>first</span> footnote
+          </span>
+          {value}
+          <span className="wb-inv"> referrer</span>
+        </a>
+      </p>
+    </dd>
+  </span>
+);
 
 export default Footnote;
