d58186d5f40a3553af510176c3151455a9611b20
YaBoiBreezy <alexbreeze01@gmail.com>
2022-05-31 11:30:22 -0400

    fixed tests

--- src/components/Pagination/Pagination.test.tsx
+++ src/components/Pagination/Pagination.test.tsx
@@ -15,19 +15,19 @@ describe('Pagination tests', () => {
     });
     test('Renders large size', () => {
       render(
-        <ul className="pagination pagination-lg">
+        <Pagination size="lg">
           {placeholder}
           <Pagination.Button />
-        </ul>
+        </Pagination>
       );
       expect(screen.getByText(placeholder)).toHaveClass('pagination-lg');
     });
     test('Renders small size', () => {
       render(
-        <ul className="pagination pagination-sm">
+        <Pagination size="sm">
           {placeholder}
           <Pagination.Button />
-        </ul>
+        </Pagination>
       );
       expect(screen.getByText(placeholder)).toHaveClass('pagination-sm');
     });
@@ -36,38 +36,25 @@ describe('Pagination tests', () => {
     test('Test active', () => {
       render(
         <Pagination>
-          <li>
-            <a href="#a" rel="prev">
-              Previous
-            </a>
-          </li>
-          <li className="active">
+          <Pagination.Button value="prev" />
+          <Pagination.Button active value="1">
             {placeholder}
-            <a href="#a">
-              1 <span className="wb-inv">Go to Page 1</span>
-            </a>
-          </li>
+          </Pagination.Button>
         </Pagination>
       );
-      expect(screen.getByText(placeholder)).toHaveClass('active');
+      expect(screen.getByText(placeholder).parentNode).toHaveClass('active');
     });
     test('Test disabled', () => {
       render(
         <Pagination>
-          <li className="disabled">
-            {placeholder}
-            <a href="#a" rel="prev">
-              Previous
-            </a>
-          </li>
-          <li>
-            <a href="#a">
-              1 <span className="wb-inv">Go to Page 1</span>
-            </a>
-          </li>
+          {placeholder}
+          <Pagination.Button disabled value="next" />
+          <Pagination.Button value="1" />
         </Pagination>
       );
-      expect(screen.getByText(placeholder)).toHaveClass('disabled');
+      expect(screen.getByText(placeholder).childNodes[1]).toHaveClass(
+        'disabled'
+      );
     });
   });
 });
