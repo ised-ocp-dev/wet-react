07cac57145b39f13eafb80d5fce4627ad4755bef
YaBoiBreezy <alexbreeze01@gmail.com>
2022-06-10 10:54:16 -0400

    lightbox and gallery with bootstrap, needs some stuff

--- src/components/Lightbox/Lightbox.tsx
+++ src/components/Lightbox/Lightbox.tsx
@@ -1,145 +1,68 @@
 import React from 'react';
+import Modal from '@components/Modal';
 import '../../style.css';
 
 export interface LightboxProps extends React.HTMLAttributes<HTMLElement> {
-  /** link(s) to the lightbox(es) image(s) */
-  src?: string;
-  /** content of Lightbox */
-  children?: React.ReactNode;
   /** title of lightbox */
   title?: string;
+  /** link(s) to the lightbox(es) image(s) */
+  src?: string;
   /** use to hide lightbox, recommended for galleries where you want to only show the first image */
   hidden?: boolean;
-}
-
-function closeLightbox(e: Element) {
-  const main = e.closest('.lightbox-breezy');
-  if (main) {
-    const html = main.closest('html');
-    if (html) {
-      const style = html.getAttribute('style');
-      if (style) {
-        html.setAttribute(
-          'style',
-          style.replace('margin-right: 17px; overflow: hidden;', 'ty')
-        );
-      }
-    }
-    const body = main.closest('body');
-    if (body) {
-      body.classList.remove('mfp-zoom-out-cur');
-      body.classList.remove('wb-modal');
-    }
-  }
-  window.scrollBy(0, -1 * (window.innerHeight / 2));
+  /** content of Lightbox */
+  children?: React.ReactNode;
 }
 
 const Lightbox = ({
-  children,
   title = '',
   src = '',
   hidden = false,
+  children,
 }: LightboxProps) => {
-  const [open, setOpen] = React.useState(false);
+  const [isOpen, setIsOpen] = React.useState(false);
   return (
     <span className="lightbox-breezy">
       <a
-        className="wb-lbx wb-init wb-lbx-inited"
         href={src}
         title={title}
         hidden={hidden}
         onClick={(e) => {
           e.preventDefault();
-          if ((e.target as Element).closest('.wb-lbx') != null) {
-            setOpen(true);
-            (e.target as Element)
-              .closest('html')
-              ?.setAttribute('style', 'margin-right: 17px; overflow: hidden;');
-            const body = (e.target as Element).closest('body');
-            if (body) {
-              body.className = 'mfp-zoom-out-cur wb-modal';
-            }
-            const sib2 = (e.target as Element).closest('.lightbox-breezy')
-              ?.childNodes[2];
-            if (sib2) {
-              window.setTimeout(
-                // scroll doesn't work bc based on image, doesn't take effect until this function ends,
-                // so I added a 0ms timeout and set to smooth to cover up 1-frame spasm
-                () =>
-                  (
-                    sib2.childNodes[0].childNodes[0].childNodes[0] as Element
-                  ).scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'center',
-                    inline: 'center',
-                  }),
-                0
-              );
-            }
-            document.addEventListener('keydown', (event) => {
-              if (event.key === 'Escape') {
-                closeLightbox(e.target as Element);
-                setOpen(false);
-              }
-            });
+          const gallery = (e.target as Element).closest('.lbx-gal');
+          if (!gallery) {
+            setIsOpen(true);
           }
         }}
       >
         {children}
       </a>
-      <div
-        role="link"
-        tabIndex={0}
-        aria-label="Close Lightbox"
-        className={open ? 'mfp-bg mfp-ready' : ''}
-        onClick={(e1) => {
-          closeLightbox(e1.target as Element);
-          setOpen(false);
-        }}
-        onKeyDown={() => {
-          // ignore, handled elsewhere
-        }}
-      />
-      <div
-        className="mfp-close-btn-in mfp-auto-cursor mfp-ready"
-        style={{
-          overflow: 'hidden auto',
-          height: open ? `${window.innerHeight}px` : 'auto',
-        }}
+      <Modal
+        show={isOpen}
+        centered // supposed to center it, but css class doesn't exist...
+        animation={false}
+        backdropClassName="mfp-zoom-out-cur" // supposed to add cursor: zoom-out, but broken bc it's behind lightbox div (siblings, lower z-index)
+        onHide={() => setIsOpen(false)}
+        className="mfp-zoom-out-cur"
       >
-        <dialog className="mfp-container" open={open}>
-          <div className="mfp-container mfp-s-ready mfp-image-holder">
-            <div
-              className="mfp-content"
-              aria-labelledby="lbx-title"
-              data-pgtitle="Lightbox"
-            >
-              <div className="mfp-figure">
-                <button
-                  title="Close overlay (escape key)"
-                  type="button"
-                  className="mfp-close"
-                  onClick={(e1) => {
-                    closeLightbox(e1.target as Element);
-                    setOpen(false);
-                  }}
-                >
-                  ×<span className="wb-inv">Close overlay (escape key)</span>
-                </button>
-                <figure>
-                  <img className="mfp-img" alt="hello" src={src} />
-                  <figcaption>
-                    <div className="mfp-bottom-bar" id="lbx-title">
-                      <div className="mfp-title">{title}</div>
-                      <div className="mfp-counter"> </div>
-                    </div>
-                  </figcaption>
-                </figure>
-              </div>
+        <Modal.Body bsPrefix="bg-darker" style={{ cursor: 'auto' }}>
+          <button
+            title="Close overlay (escape key)"
+            type="button"
+            className="mfp-close"
+            onClick={() => setIsOpen(false)}
+            style={{ width: 'auto' }}
+          >
+            ×<span className="wb-inv"> Close overlay (escape key)</span>
+          </button>
+          <img className="mfp-img" src={src} alt="alternate" />
+          <figcaption>
+            <div className="mfp-bottom-bar" id="lbx-title">
+              <div className="mfp-title">{title}</div>
+              <div className="mfp-counter" />
             </div>
-          </div>
-        </dialog>
-      </div>
+          </figcaption>
+        </Modal.Body>
+      </Modal>
     </span>
   );
 };
--- src/components/Lightbox/LightboxGallery.tsx
+++ src/components/Lightbox/LightboxGallery.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import Modal from '@components/Modal';
 import '../../style.css';
 
 export interface LightboxGalleryProps
@@ -11,121 +12,27 @@ export interface LightboxGalleryProps
   id: string;
 }
 
-function galleryCloseLightbox(e: Element) {
-  const html = e.closest('html');
-  if (html) {
-    const style = html.getAttribute('style');
-    if (style) {
-      html.setAttribute(
-        'style',
-        style.replace('margin-right: 17px; overflow: hidden;', '')
-      );
-    }
-  }
-  const body = e.closest('body');
-  if (body) {
-    body.classList.remove('mfp-zoom-out-cur');
-    body.classList.remove('wb-modal');
-  }
-}
-
-function getNextPrev(hop: number, e: Element) {
-  window.setTimeout(
-    () =>
-      (
-        e.closest('.lbx-gal')?.childNodes[1].childNodes[0].childNodes[0]
-          .childNodes[0].childNodes[0] as Element
-      ).scrollIntoView({
-        behavior: 'auto',
-        block: 'center',
-        inline: 'center',
-      }),
-    0
-  );
-}
-
-function galleryOpenLightbox(event: Event, id: string) {
-  const e = event.target as Element;
-  const gallery = document.getElementById(id);
-  if (!gallery) {
-    return;
-  }
-  e.closest('html')?.setAttribute(
-    'style',
-    'margin-right: 17px; overflow: hidden;'
-  );
-  const body = e.closest('body');
-  if (body) {
-    body.className = 'mfp-zoom-out-cur wb-modal';
-  }
-  window.setTimeout(
-    () =>
-      (
-        gallery.childNodes[1].childNodes[0].childNodes[0].childNodes[0]
-          .childNodes[0] as Element
-      ).scrollIntoView({
-        behavior: 'smooth',
-        block: 'center',
-        inline: 'center',
-      }),
-    0
-  );
-}
-
 const LightboxGallery = ({
   children,
   tag = 'section',
   id,
   ...rest
 }: LightboxGalleryProps) => {
-  const [open, setOpen] = React.useState(false);
+  const [isOpen, setIsOpen] = React.useState(false);
   const [index, setIndex] = React.useState(-1);
-  const temp = document.getElementById(id);
-  const numImages =
-    temp === null ? 0 : temp.getElementsByClassName('lightbox-breezy').length;
+  const [images, setImages] = React.useState<HTMLCollectionOf<Element>>();
+  const [numImages, setNumImages] = React.useState(0);
+
   function tempOpen(e: Event) {
-    galleryOpenLightbox(e, id);
-    setOpen(true);
-    document.addEventListener('keydown', (ev) => {
-      if (ev.key === 'Escape') {
-        setOpen(false);
-        galleryCloseLightbox(ev.target as Element);
-      }
-    });
-    document.addEventListener('keydown', (ev) => {
-      if (ev.key === 'ArrowLeft') {
-        const newIndex = (index - 1) % numImages;
-        setIndex(newIndex < 0 ? numImages - 1 : newIndex);
-        window.setTimeout(
-          () =>
-            (
-              (ev.target as Element).closest('.lbx-gal')?.childNodes[1]
-                .childNodes[0].childNodes[0].childNodes[0]
-                .childNodes[0] as Element
-            ).scrollIntoView({
-              behavior: 'auto',
-              block: 'center',
-              inline: 'center',
-            }),
-          0
-        );
-      }
-    });
-    document.addEventListener('keydown', (ev) => {
-      if (ev.key === 'ArrowRight') {
-        setIndex((index + 1) % numImages);
-        console.log(index);
-        getNextPrev(1, ev.target as Element);
-      }
-    });
-    const temp2 = Number(
+    // open gallery, find index of lightbox to open
+    setIsOpen(true);
+    const newIndex = Number(
       (
-        (e.target as Element).closest('.lightbox-breezy')
-          ?.childNodes[0] as Element
+        (e.target as Element).closest('.lightbox-breezy') as Element
       ).getAttribute('index')
     );
-    if (temp2 || temp2 === 0) {
-      setIndex(temp2);
+    if (newIndex || newIndex === 0) {
+      setIndex(newIndex);
     }
   }
 
@@ -133,128 +40,111 @@ const LightboxGallery = ({
     const linkList = document
       .getElementById(id)
       ?.getElementsByClassName('lightbox-breezy');
+    // on load, go through children to find lightboxes. Give each an index and callback function to trigger gallery
     if (linkList) {
       for (let i = 0; i < linkList.length; i += 1) {
-        (linkList[i].childNodes[0] as Element).classList.remove('wb-lbx');
-        (linkList[i].childNodes[0] as Element).setAttribute('index', `${i}`);
+        (linkList[i] as Element).setAttribute('index', `${i}`);
         (linkList[i].childNodes[0] as HTMLElement).addEventListener(
           'click',
           (e) => tempOpen(e),
           false
         );
       }
+      // get lightboxes for future reference
+      const temp = document.getElementById(id);
+      if (temp) {
+        setImages(temp.getElementsByClassName('lightbox-breezy'));
+        setNumImages(temp.getElementsByClassName('lightbox-breezy').length);
+      }
     }
+    // arrow key handlers, SUPER broken
+    /*    document.addEventListener('keydown', (ev) => {
+      ev.preventDefault();
+      ev.stopPropagation();
+      if (ev.key === 'ArrowUp') {
+        console.log('u');
+        const newIndex = (index - 1) % numImages;
+        setIndex(newIndex < 0 ? numImages - 1 : newIndex);
+      } else if (ev.key === 'ArrowDown') {
+        console.log('d');
+        const hi = (index + 1) % numImages;
+        setIndex(hi);
+      }
+      return false;
+    }); */
   }, []);
 
   return (
-    <span className="lbx-gal wb-init wb-lbx-inited" id={id}>
-      <div
-        role="link"
-        tabIndex={0}
-        aria-label="Close Lightbox"
-        className={open ? 'mfp-bg mfp-ready' : ''}
-        onClick={(e1) => {
-          setOpen(false);
-          galleryCloseLightbox(e1.target as Element);
-        }}
-        onKeyDown={() => {
-          // ignore, handled elsewhere
-        }}
-      />
-      <div
-        className="mfp-close-btn-in mfp-auto-cursor mfp-ready"
-        style={{
-          overflow: 'hidden auto',
-          height: open ? `${window.innerHeight}px` : 'auto',
-        }}
+    <span className="lbx-gal" id={id}>
+      <Modal
+        show={isOpen}
+        centered // supposed to center it, but css class doesn't exist...
+        animation={false}
+        backdropClassName="mfp-zoom-out-cur" // supposed to add cursor: zoom-out, but broken bc it's behind lightbox div (siblings, lower z-index)
+        onHide={() => setIsOpen(false)}
+        className="mfp-zoom-out-cur"
       >
-        <dialog className="mfp-container" open={open}>
-          <div className="mfp-container mfp-s-ready mfp-image-holder">
-            <div
-              className="mfp-content"
-              aria-labelledby="lbx-title"
-              data-pgtitle="Lightbox"
-            >
-              <div className="mfp-figure">
-                <button
-                  title="Close overlay (escape key)"
-                  type="button"
-                  className="mfp-close"
-                  onClick={(e1) => {
-                    setOpen(false);
-                    galleryCloseLightbox(e1.target as Element);
-                  }}
-                >
-                  ×<span className="wb-inv">Close overlay (escape key)</span>
-                </button>
-                <figure>
-                  <img
-                    className="mfp-img"
-                    src={
-                      (document
-                        .getElementById(id)
-                        ?.getElementsByClassName('lightbox-breezy')[index]
-                        ?.childNodes[0] as Element)
-                        ? (
-                            document
-                              .getElementById(id)
-                              ?.getElementsByClassName('lightbox-breezy')[index]
-                              ?.childNodes[0] as Element
-                          ).getAttribute('href')
-                        : ''
-                    }
-                    alt="hello"
-                  />
-                  <figcaption>
-                    <div className="mfp-bottom-bar" id="lbx-title">
-                      <div className="mfp-title">
-                        {(document
-                          .getElementById(id)
-                          ?.getElementsByClassName('lightbox-breezy')[index]
-                          ?.childNodes[0] as Element)
-                          ? (
-                              document
-                                .getElementById(id)
-                                ?.getElementsByClassName('lightbox-breezy')[
-                                index
-                              ]?.childNodes[0] as Element
-                            ).getAttribute('title')
-                          : ''}
-                      </div>
-                      <div className="mfp-counter">{`${
-                        index + 1
-                      }/${numImages}`}</div>
-                    </div>
-                  </figcaption>
-                </figure>
+        <Modal.Body bsPrefix="bg-darker" style={{ cursor: 'auto' }}>
+          <button
+            title="Close overlay (escape key)"
+            type="button"
+            className="mfp-close"
+            onClick={() => setIsOpen(false)}
+            style={{ width: 'auto' }}
+          >
+            ×<span className="wb-inv"> Close overlay (escape key)</span>
+          </button>
+          <img
+            className="mfp-img"
+            src={
+              // couldn't assign string|null to src, so I convert it to string, that way null becomes "null" lol
+              images !== undefined && images[index]
+                ? String(
+                    (images[index].childNodes[0] as Element).getAttribute(
+                      'href'
+                    )
+                  )
+                : ''
+            }
+            alt="alternate"
+          />
+          <figcaption>
+            <div className="mfp-bottom-bar" id="lbx-title">
+              <div className="mfp-title">
+                {images !== undefined && images[index]
+                  ? (images[index].childNodes[0] as Element).getAttribute(
+                      'title'
+                    )
+                  : ''}
+              </div>
+              <div className="mfp-counter">
+                Item {index + 1} of {numImages}
               </div>
             </div>
-            <button
-              title="Previous (left arrow key)"
-              type="button"
-              className="mfp-arrow mfp-arrow-left mfp-prevent-close"
-              onClick={(e) => {
-                const newIndex = (index - 1) % numImages;
-                setIndex(newIndex < 0 ? numImages - 1 : newIndex);
-                getNextPrev(-1, e.target as Element);
-              }}
-            >
-              <span className="wb-inv"> Previous (left arrow key)</span>
-            </button>
-            <button
-              title="Next (right arrow key)"
-              type="button"
-              className="mfp-arrow mfp-arrow-right mfp-prevent-close"
-              onClick={(e) => {
-                setIndex((index + 1) % numImages);
-                getNextPrev(1, e.target as Element);
-              }}
-            >
-              <span className="wb-inv"> Next (right arrow key)</span>
-            </button>
-          </div>
-        </dialog>
-      </div>
+          </figcaption>
+          <button
+            title="Previous (left arrow key)"
+            type="button"
+            className="mfp-arrow mfp-arrow-left mfp-prevent-close"
+            onClick={() => {
+              const newIndex = (index - 1) % numImages;
+              setIndex(newIndex < 0 ? numImages - 1 : newIndex);
+            }}
+          >
+            <span className="wb-inv"> Previous (left arrow key)</span>
+          </button>
+          <button
+            title="Next (right arrow key)"
+            type="button"
+            className="mfp-arrow mfp-arrow-right mfp-prevent-close"
+            onClick={() => {
+              setIndex((index + 1) % numImages);
+            }}
+          >
+            <span className="wb-inv"> Next (right arrow key)</span>
+          </button>
+        </Modal.Body>
+      </Modal>
       {React.createElement(tag, { ...rest }, children)}
     </span>
   );
