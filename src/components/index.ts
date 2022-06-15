b36dda6b753d4351a91b3c76ad9e73c99dda24a9
YaBoiBreezy <91281967+YaBoiBreezy@users.noreply.github.com>
2022-06-15 15:07:20 -0400

    removed grid import 1

--- src/components/index.ts
+++ src/components/index.ts
@@ -19,7 +19,6 @@ export { default as Table } from './Table';
 export { default as Modal } from './Modal';
 export { default as InputGroup } from './InputGroup';
 export { default as Heading } from './Heading';
-export { default as Quotation } from './Quotation';
-export { default as Grid } from './Grid';
+export { default as Quotation } from './Quotation';\
 export { default as List } from './List';
 export { default as Grid } from './Grid';
